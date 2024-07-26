39ef8f28f6c01f534b8fd5f478c38d17dbb12154
KiraraM <3242814866@qq.com>
2024-07-26 16:09:57 +0800

    twa train in .py

--- train_dldr.py
+++ train_dldr.py
@@ -1,5 +1,7 @@
 # main.py
-from trainer.train_twa_ddp import get_parser, start_ddp
+from trainer.train_twa_ddp import get_parser, set_seed, main
 if __name__ == '__main__':
     parser = get_parser()
-    start_ddp(parser)
+    args = parser.parse_args()
+    set_seed(args.randomseed)
+    main(args)
