fa66fde041514afe72dde47efb02ac806954f593
KiraraM <3242814866@qq.com>
2024-07-27 09:43:57 +0800

    delete some useless

--- mian.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from trainer.train_sgd_imagenet import main
-
-if __name__ == '__main__':
-    main()
--- test_ad_train.py
+++
@@ -1,5 +0,0 @@
-# main.py
-from trainer.adversarial_train import main
-
-if __name__ == '__main__':
-    main()
