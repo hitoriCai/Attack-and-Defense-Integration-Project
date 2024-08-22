5bcb98ceab607f34659b9feba8ea97b0ac5d5f16
windcoat <958493843@q.com>
2024-08-22 14:27:50 +0800

    add readme

--- normal_training.sh
+++ normal_training.sh
@@ -4,10 +4,12 @@ device=0,1
 model=resnet101
 path=/opt/data/common/ILSVRC2012/
 epochs=90
+DST=eps_8_save_$model
+eps=8
 CUDA_VISIBLE_DEVICES=$device  python3 normal_training.py -a $model \
     --epochs $epochs --workers 8  --dist-url 'tcp://127.0.0.1:1234' \
     --dist-backend 'nccl' --multiprocessing-distributed \
-    --world-size 1 --rank 0 $path --save_dir eps_8_save_$model --eps 8
+    --world-size 1 --rank 0 $path --save_dir $DST --eps $eps
 
 # with --eps $eps,  meaning adversarial training
 #datasets=ImageNet
